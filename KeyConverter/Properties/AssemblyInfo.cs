308ffaca406d96653e0ee4bde5d655c29756f82f
ARM <leiomama117@gmail.com>
2023-03-09 16:37:22 +0900

    Change in version control method `v1.0.2`

--- KeyConverter/Properties/AssemblyInfo.cs
+++ KeyConverter/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("KeyConverter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("3ds KeyConverter")]
+[assembly: AssemblyDescription("This tool allows you to convert and reverse 3DS key codes.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KeyConverter")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("3ds KeyConverter")]
+[assembly: AssemblyCopyright("Copyright ©アーム 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
---
+++ KeyConverter/Utils/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace KeyConverter.Utils
+{
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// 16進文字列か判定する
+        /// </summary>
+        /// <returns>16進文字列の場合trueを返す。そうでない場合はfalseを返す。</returns>
+        public static bool IsHexString(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+            foreach (char c in str)
+            {
+                if (!Uri.IsHexDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
